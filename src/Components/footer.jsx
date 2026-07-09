d19765b4f548e731e328a453164466fd5050d131
Valerius <valeriusdecrypto@gmail.com>
2026-07-09 13:13:51 +0100

    updated footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -4,7 +4,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h3>Made by Val   | |   &copy; 2026 All rights reserved.</h3>
+      <h3> Designed & Developed by Val   | |   &copy; 2026 All rights reserved.</h3>
     </div>
   );
 };
